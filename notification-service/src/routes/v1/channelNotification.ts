46dc0b8d088e27807aa29a95737cbfc87b9fd34a
terryli0095 <terryli0095@gmail.com>
2022-03-14 19:23:14 -0700

    add logging

--- notification-service/src/routes/v1/channelNotification.ts
+++ notification-service/src/routes/v1/channelNotification.ts
@@ -2,21 +2,25 @@ import express from 'express'
 import client from '../../discordClient'
 import { TextChannel, MessageEmbed } from 'discord.js'
 import { ChannelNotificationError } from '../../lib/error'
+import logger from '../../config/logger'
 
 const router = express.Router()
 
 router.post('/push', function (req, res) {
 	const guildId = req.body?.guild_id
 	const channelId = req.body?.channel_id
+	logger.info(`channel push ${guildId} ${channelId}`)
 	const channel = client?.channels.cache.get(channelId)
 	if (!channel) {
 		const guild = client?.guilds.cache.get(guildId)
 		if (!guild) { // this stuff is actually probably not needed
 			// throw new ChannelNotificationError('wonder bot no longer connected to guild')
+			logger.error(`wonder bot no longer connected to guild ${guildId}`)
 			res.status(400).json({ error: 'wonder bot no longer connected to guild' })
 			return
 		}
 		// throw new ChannelNotificationError('channel id no longer exist')
+		logger.error(`channel id no longer exist ${channelId}`)
 		res.status(400).json({ error: 'channel id no longer exist' })
 		return
 	}
@@ -26,7 +30,7 @@ router.post('/push', function (req, res) {
 	const url = req.body?.url
 	const embed = formatDiscordChannelMessage(title, description, url)
 	const textChannel = channel as TextChannel
-	console.log('message', message)
+	logger.info('message', message)
 	textChannel.send({ content: message, embeds: [embed] })
 	res.send('ok')
 })
