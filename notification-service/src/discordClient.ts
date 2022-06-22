4a2e7ae20c7279ba35c723789fd759cebec7d8a7
terryli0095 <terryli0095@gmail.com>
2022-06-22 14:46:38 -0500

    add to intent

--- notification-service/src/discordClient.ts
+++ notification-service/src/discordClient.ts
@@ -2,7 +2,7 @@ import { Client, Intents } from 'discord.js'
 import config from './config/config'
 
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS]
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES]
 })
 
 client.login(config.WONDER_DISCORD_BOT_TOKEN)
